bfe25b80971059b2eef3c7778f56a3a3e095540a
Michael Hladky <michael@hladky.at>
2018-09-02 21:23:49 +0200

    setup prerendering

--- server.ts
+++ server.ts
@@ -44,10 +44,6 @@ app.get('*', (req, res) => {
   res.render('index', { req });
 });
 
-app.get('*', (req, res) => {
-  res.render('index', { req });
-});
-
 // Start up the Node server
 app.listen(PORT, () => {
   console.log(`Node Express server listening on http://localhost:${PORT}`);
